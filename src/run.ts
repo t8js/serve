01f418134823749e72204c4a13583380d8becc00
axtk <axtkx@inbox.ru>
2025-09-24 22:45:36 +0300

    add build option

--- src/run.ts
+++ src/run.ts
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
+import { exec as originalExec } from "node:child_process";
+import { join } from "node:path";
+import { promisify } from "node:util";
 import { serve } from "./serve";
 
-let args = process.argv.slice(2);
+const exec = promisify(originalExec);
 
 /**
  * @example
  * serve 3000 app public dist
  * serve 127.0.0.1:3000 app public dist
+ * serve 3000 app public dist -b
+ * serve 3000 app public dist -b src/index.ts
  */
-serve({
-  url: args[0],
-  path: args[1],
-  dirs: args.slice(2),
-});
+async function run() {
+  let [url, path = "", ...args] = process.argv.slice(2);
+  let buildFlagIndex = args.indexOf("-b");
+
+  if (buildFlagIndex !== -1) {
+    let inputFile = join(path, args[buildFlagIndex + 1] ?? "index.ts");
+    let outputFile = join(path, "dist", args[buildFlagIndex + 2] ?? "index.js");
+
+    await exec(`npx esbuild ${inputFile} --outfile=${outputFile} --bundle --platform=neutral --log-level=warning`);
+  }
+
+  serve({
+    url,
+    path,
+    dirs: buildFlagIndex === -1 ? args : args.slice(0, buildFlagIndex),
+  });
+}
+
+run();
