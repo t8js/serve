0ed50aaaaebc8117ad9b38d59a323745e62b75eb
axtk <axtkx@inbox.ru>
2025-09-25 12:00:41 +0300

    rename url path

--- src/getFilePath.ts
+++ src/getFilePath.ts
@@ -5,22 +5,22 @@ import { isValidFilePath } from "./isValidFilePath";
 const cwd = process.cwd();
 
 export async function getFilePath(
-  urlPath = "",
+  url = "",
   { path = "", dirs = [], spa }: Config,
 ) {
-  let effectiveURLPath = urlPath.replace(/[?#].*$/, "");
+  let urlPath = url.replace(/[?#].*$/, "");
 
   for (let dir of dirs.length === 0 ? [""] : dirs) {
     let dirPath = join(cwd, path, dir);
-    let filePath = join(dirPath, effectiveURLPath);
+    let filePath = join(dirPath, urlPath);
 
     if (
-      !effectiveURLPath.endsWith("/") &&
+      !urlPath.endsWith("/") &&
       (await isValidFilePath(filePath, dirPath))
     )
       return filePath;
 
-    filePath = join(dirPath, spa ? "/" : effectiveURLPath, "index.html");
+    filePath = join(dirPath, spa ? "" : urlPath, "index.html");
 
     if (await isValidFilePath(filePath, dirPath)) return filePath;
   }
